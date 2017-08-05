6261bf06eadf31f385f8ff759993c861d81102c0
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-08-06 01:23:20 +0200

    feat(nuxt): implement InstantSearch

--- examples/nuxt/nuxt.config.js
+++ examples/nuxt/nuxt.config.js
@@ -34,5 +34,7 @@ module.exports = {
         })
       }
     }
-  }
+  },
+
+  plugins: ['~plugins/vue-instantsearch']
 }
---
+++ examples/nuxt/pages/search.vue
@@ -0,0 +1,37 @@
+<template>
+  <ais-index :search-store="searchStore" >
+    <ais-search-box></ais-search-box>
+    <ais-results>
+      <template scope="{ result }">
+        <h2>{{ result.name }}</h2>
+      </template>
+    </ais-results>
+  </ais-index>
+</template>
+
+<script>
+import { createFromAlgoliaCredentials, createFromSerialized } from 'vue-instantsearch'
+const searchStore = createFromAlgoliaCredentials('latency', '3d9875e51fbd20c7754e65422f7ce5e1')
+searchStore.indexName = 'bestbuy'
+
+export default {
+  async asyncData () {
+    searchStore.start()
+    await searchStore.waitUntilInSync()
+
+    const serializedSearchStore = searchStore.serialize()
+
+    return { serializedSearchStore }
+  },
+
+  data () {
+    return {
+      searchStore: null
+    }
+  },
+
+  created () {
+    this.searchStore = createFromSerialized(this.serializedSearchStore)
+  }
+}
+</script>
